cabb23a42d384e0614092f2a115906d76e8adde7
stanley <tochukwuu14@gmail.com>
2022-11-04 12:00:04 +0100

    Add component for categories

---
+++ src/components/Categories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Categories = () => {
+    <div className="catagories">
+    <button className="status_button" type="button">Check Status</button>
+  </div>
+};
+
+export default Categories;
