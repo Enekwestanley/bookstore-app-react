6d74d7e243f68f140662d7488dccaccb33400451
stanley <tochukwuu14@gmail.com>
2022-11-09 02:39:57 +0100

    Modified some JS files to yield desired result

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,15 +1,16 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import Book from './Book';
 import AddBook from './AddBook';
 
 const Books = () => {
   const bookState = useSelector((state) => state.books);
-
   return (
     <div className="books_container">
-      {bookState.map((books) => (
-        <div key={books.id}>
-          <Book author={books.author} title={books.title} id={books.id} />
+      {bookState.map((book) => (
+        <div key={book.id}>
+          <Book title={book.title} author={book.author} id={book.id} />
+
         </div>
       ))}
       <AddBook />
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,12 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import checkStatusReducer from './categories/Categories';
 import bookReducer from './books/books';
 
-const rootReducer = combineReducers({
-  books: bookReducer,
-  status: checkStatusReducer,
+const bookStore = configureStore({
+  reducer: {
+    books: bookReducer,
+    status: checkStatusReducer,
+  },
 });
-
-const bookStore = configureStore(rootReducer);
 
 export default bookStore;
