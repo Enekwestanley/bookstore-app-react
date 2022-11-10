1e91f25a472fe2ca001e3f55fba5756096fffeb9
stanley <tochukwuu14@gmail.com>
2022-11-10 20:04:51 +0100

    Add reducers abd action Configure redux store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import checkStatusReducer from './categories/Categories';
-import bookReducer from './books/books';
+import bookReducer, { getBooks } from './books/books';
 
 const bookStore = configureStore({
   reducer: {
     books: bookReducer,
     status: checkStatusReducer,
   },
-});
+}, applyMiddleware(thunk));
+bookStore.dispatch(getBooks());
 
 export default bookStore;
